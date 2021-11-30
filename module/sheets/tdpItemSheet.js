7494542891dc9eee58240f2465f5c3f9ee66dd0f
Billiam9420 <Billiam94@gmail.com>
2021-11-30 15:39:37 -0500

    Revert "Comments!"
    This reverts commit cdf2d5db98cdcc72ad8e83656200e112ef0d8c74.

--- module/sheets/tdpItemSheet.js
+++ module/sheets/tdpItemSheet.js
@@ -2,7 +2,6 @@ export default class tdpItemSheet extends ItemSheet
 {
     get template()
     {
-    	// point item types to the proper html files
         return `systems/case-system/templates/items/${this.item.data.type}-sheet.html`;
     }
 }
