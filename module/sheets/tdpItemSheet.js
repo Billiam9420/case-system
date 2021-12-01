f82213b16a27f0e601249316ba451a7cd5fdfe48
Billiam9420 <Billiam94@gmail.com>
2021-12-01 12:10:01 -0500

    Updating items

--- module/sheets/tdpItemSheet.js
+++ module/sheets/tdpItemSheet.js
@@ -4,4 +4,12 @@ export default class tdpItemSheet extends ItemSheet
     {
         return `systems/case-system/templates/items/${this.item.data.type}-sheet.html`;
     }
+
+    getData()
+    {
+        // simplify access so that we can type ".data" instead of ".data.data"
+		let sheetData = super.getData()
+		sheetData.data = sheetData.data.data
+		return sheetData;
+	}
 }
